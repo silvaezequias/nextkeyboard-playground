b8b70b40ec6baf4e853f54e7e6e19d61e112d6ab
ezequiaslopesdasilva <wyskzito@gmail.com>
2024-12-17 11:31:30 -0300

    fix: change the main page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Virtual Keyboard",
+  title: "NextKeyboard - Playground",
   description: "A customizable virtual keyboard",
 };
 
