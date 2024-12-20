fd09eead1e3bce35839817a53358893ef56afb05
ezequiaslopesdasilva <wyskzito@gmail.com>
2024-12-20 11:40:57 -0300

    fix: add toastProvider to hoot component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { ToastProvider } from "@/components/Toast/ToastContext";
 import { VirtualKeyboard } from "@/components/VirtualKeyboard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">Teclado Virtual</h1>
-      <VirtualKeyboard />
+      <ToastProvider>
+        <VirtualKeyboard />
+      </ToastProvider>
     </main>
   );
 }
