09b031b04f06c4cd298a0c5e4fd7065891e451b2
ezequiaslopesdasilva <wyskzito@gmail.com>
2024-12-20 11:41:30 -0300

    fix: change main page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { VirtualKeyboard } from "@/components/VirtualKeyboard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Teclado Virtual</h1>
+      <h1 className="text-4xl font-bold mb-8">NextKeyboard - Playground</h1>
       <ToastProvider>
         <VirtualKeyboard />
       </ToastProvider>
